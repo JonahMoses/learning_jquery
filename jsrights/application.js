042e5f51275b4913645651e727b58f48aad61394
Jonah <Jonahkmoses@gmail.com>
2013-11-20 14:01:41 -0700

    added stylesheet

---
+++ jsrights/application.js
@@ -0,0 +1,7 @@
+$(function (){
+  var back_to_top = "<a href='#top'>Back to Top</a>";
+  $("div.article").append(back_to_top);
+
+  var tableOfContents = "<h2 id='toc'><a name='toc'>Table of Contents</a></h2>";
+  $("").after(tableOfContents)
+});
