afd288e0db06a286b8601e1fa6823712ded821c2
Jonah <Jonahkmoses@gmail.com>
2013-11-21 11:52:04 -0700

    refactored into a plugin

--- jsrights/application.js
+++ jsrights/application.js
@@ -1,15 +1,20 @@
-$(function (){
+$.fn.tableOfContents = function(header) {
+
   var backToTop = "<a href='#top'>Back to Top</a>";
-  $("div.article").append(backToTop);
+  $(this).append(backToTop);
 
   var tableOfContents = "<h2 id='toc'><a name='toc'>Table of Contents</a></h2>";
-  $("h1:first").after(tableOfContents);
+  $(header).after(tableOfContents);
 
   var unorderedList = "<ul name='toc'></ul>";
-  $("#header").after(unorderedList);
+  $(header).after(unorderedList);
 
-  $("div.article h2").css('background', '#CCF');
-  $("div.article h2").each(function(){
+  var h2 = $(this).find("h2");
+
+  h2.css('background', '#CCF');
+  // $("h2", this).css('background', '#CCF');
+
+  h2.each(function(){
     var title = $(this).text();
     console.log(title);
     var url = title.trim().toLowerCase().replace(" ","_");
@@ -27,5 +32,8 @@ $(function (){
     });
     $(this).after(toggleLink);
   });
+};
 
+$(function (){
+    $("div.article").tableOfContents($('h1'));
 });
