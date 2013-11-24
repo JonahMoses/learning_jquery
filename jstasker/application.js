c9f9755038138c38d985740e54d48ff3f64731e6
Jonah <Jonahkmoses@gmail.com>
2013-11-24 16:15:35 -0700

    autofocus and clearing of field when submitted

--- jstasker/application.js
+++ jstasker/application.js
@@ -1,9 +1,12 @@
 $(function(){
+
   $('form#add_task').on('submit', function(event){
     event.preventDefault();
     var taskText = $('input#task_text').val ();
-    console.log(taskText)
     var listItem = "<li>" + taskText + "</li>";
     $('#tasks ul').append(listItem);
+    $('input#task_text').val ("")
   });
+
+
 });
